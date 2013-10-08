7b6e4ae146e876752208dcb8411f2454007385af
tritlo <mpg@mpg.is>
2013-10-08 23:25:51 +0000

    Went through the Android-studio interface, to see if it works. It does!

--- rUglanProject/rUglan/src/instrumentTest/java/is/mpg/ruglan/test/iCalParserTest.java
+++ rUglanProject/rUglan/src/instrumentTest/java/is/mpg/ruglan/test/iCalParserTest.java
@@ -1,16 +1,41 @@
 package is.mpg.ruglan.test;
-import is.mpg.ruglan.iCalParser;
+
 import android.test.AndroidTestCase;
+import is.mpg.ruglan.iCalParser;
 
 /**
  * Created by tritlo on 10/8/13.
+ * @author Matti
  */
-public class iCalParserTest extends AndroidTestCase{
+public class iCalParserTest extends AndroidTestCase {
 
-    //@Test
+    public void setUp() throws Exception {
+        super.setUp();
+    }
+
+    public void testUrlToCalEvents() throws Exception {
+
+    }
+
     public void testUrlToString() throws Exception {
         String hws = "hello, world\n";
         String urlHWS = iCalParser.urlToString("https://notendur.hi.is/mpg3/helloworld.html");
         assertEquals(hws,urlHWS);
+    }
+
+    public void testParseCalendar() throws Exception {
+
+    }
+
+    public void testGetICalValue() throws Exception {
+
+    }
+
+    public void testRemoveCR() throws Exception {
+
+    }
+
+    public void testCalendarToEventList() throws Exception {
+
     }
 }
