103ae99b2a19baec0f0f7e07f7cec1cdfaeeee75
Sigurdur Fannar Vilhelmsson <siggifv@gmail.com>
2013-10-10 18:17:36 +0000

    Html view in HomeActivity

--- rUglanProject/rUglan/src/main/java/is/mpg/ruglan/HomeActivity.java
+++ rUglanProject/rUglan/src/main/java/is/mpg/ruglan/HomeActivity.java
@@ -1,8 +1,11 @@
 package is.mpg.ruglan;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -16,37 +19,54 @@ public class HomeActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        WebView wv = (WebView) findViewById(R.id.webView);
 
-        ListView eventsListView = (ListView) findViewById(R.id.events_list);
+        String html =
+                "<!doctype html>\n" +
+                "<html lang=\"is\">\n" +
+                "\t<head>\n" +
+                "\t\t<meta charset=\"utf-8\">\n" +
+                "\t\t<title>Transforms</title>\n" +
+                "\t\t<style>\n" +
+                "\t\tdiv { width: 300px; height: 300px; background: pink; text-align: center; font-size: 5em; padding-top: 100px; margin: 100px 0 0 100px;}\n" +
+                "\n" +
+                "\t\tdiv\n" +
+                "\t\t{\n" +
+                "\t\t\tborder: 2px solid #000;\n" +
+                "\t\t\tborder-radius: 1em/1em;\n" +
+                "\t\t}\n" +
+                "\t\t</style>\n" +
+                "\t</head>\n" +
+                "\t<body>\n" +
+                "\t\t<div>Rúnuð horn!</div>\n" +
+                "\t</body>\n" +
+                "</html>";
+        
 
-        Date d1 = new Date(113, 9, 15, 12, 0);
-        Date d2 = new Date(113, 9, 15, 13, 0);
-        Date d3 = new Date(113, 9, 15, 14, 0);
-        Date d4 = new Date(113, 9, 15, 15, 0);
-        CalEvent [] events = new CalEvent[0];
-        try{
-            events = new iCalParser().execute("http://uc-media.rhi.hi.is/HTSProxies/6566792d312d36362e2f313436.ics").get();
-        } catch (Exception ex) {
-            events = new CalEvent[] {
-                    new CalEvent("A", "d1", "VR-II", d1, d2),
-                    new CalEvent("B", "f", "HT-104", d3, d4)
-            };
-        }
+        wv.getSettings().setJavaScriptEnabled(true);
+        wv.loadData(html, "text/html", "UTF-8");
 
-        String[] eventStrings = new String[events.length];
-        for (int i=0; i<eventStrings.length; i++) {
-            CalEvent e = events[i];
-            eventStrings[i] = e.getName() + "\n" + e.getDescription() + " "
-                    + e.getDurationString() + " @ " +e.getLocation();
-        }
-
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_1, android.R.id.text1, eventStrings);
-
-        eventsListView.setAdapter(adapter);
-
+        wv.setWebViewClient(new WebViewClient(){
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                if(isURLMatching(url)) {
+                    openNextActivity();
+                    return true;
+                }
+                return super.shouldOverrideUrlLoading(view, url);
+            }
+        });
     }
 
+    protected boolean isURLMatching(String url) {
+        // some logic to match the URL would be safe to have here
+        return true;
+    }
+
+    protected void openNextActivity() {
+        Intent intent = new Intent(this, CalEventActivity.class);
+        startActivity(intent);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
