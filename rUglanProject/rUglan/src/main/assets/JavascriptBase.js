e888a731c2454cb57ecd1c04a0dc82831bdeb128
Sigurdur Fannar Vilhelmsson <siggifv@gmail.com>
2013-10-29 07:42:35 +0000

    Better header and no margin on WebView

--- rUglanProject/rUglan/src/main/assets/JavascriptBase.js
+++ rUglanProject/rUglan/src/main/assets/JavascriptBase.js
@@ -13,11 +13,37 @@ $(document).ready(function() {
 		slotEventOverlap: false,
 		header:
 		{
-			left: 'prev,next today',
+			left: 'month,agendaWeek,agendaDay',
 			center: 'title',
-			right: 'month,agendaWeek,agendaDay'
+			right: 'prev,today,next'
+		},
+		titleFormat: {
+		    month: 'MMMM yyyy',
+		    week: "d. MMM[ yyyy]{ '&#8212;'[ d. MMM] yyyy}",
+		    day: 'dddd<br>d. MMM yyyy'
+		},
+		monthNames: ['Janúar', 'Febrúar', 'Mars', 'Apríl', 'Maí', 'Júní', 'Júlí', 'Ágúst', 'September', 'Október', 'Nóvember', 'Desember'],
+		monthNamesShort: ['Jan', 'Feb', 'Mar', 'Apr', 'Maí', 'Jún', 'Júl', 'Ágú', 'Sep', 'Okt', 'Nóv', 'Des'],
+		dayNames: ['Sunnudagur', 'Mánudagur', 'Þriðjudagur', 'Miðvikudagur', 'Fimmtudagur', 'Föstudagur', 'Laugardagur'],
+		dayNamesShort: ['Sun', 'Mán', 'Þri', 'Mið', 'Fim', 'Fös', 'Lau'],
+		buttonText: {
+		    prev:     '&lsaquo;',
+		    next:     '&rsaquo;',
+		    prevYear: '&laquo;',
+		    nextYear: '&raquo;',
+		    today:    'Í dag',
+		    month:    'Mánuður',
+		    week:     'Vika',
+		    day:      'Dagur'
+		},
+		columnFormat: {
+		    month: 'ddd',
+		    week: 'd. ddd',
+		    day: 'dddd d. MMM'
 		},
 		defaultView: 'agendaWeek',
 		%%%EVENTS%%%
 	})
+	$(".fc-header").css("width", "auto");
+	$(".fc-header").css("margin", "auto");
 });
