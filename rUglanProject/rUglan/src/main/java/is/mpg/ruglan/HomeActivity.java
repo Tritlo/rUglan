1c6387aa673029fd6b34081e84d6e0338a3dbbcb
Sigurdur Fannar Vilhelmsson <siggifv@gmail.com>
2013-10-31 14:03:30 +0000

    Process bar while loading webview

--- rUglanProject/rUglan/src/main/java/is/mpg/ruglan/HomeActivity.java
+++ rUglanProject/rUglan/src/main/java/is/mpg/ruglan/HomeActivity.java
@@ -13,8 +13,11 @@ import android.preference.PreferenceManager;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import java.io.ByteArrayOutputStream;
@@ -40,21 +43,24 @@ public class HomeActivity extends Activity {
         sContext = this;
         WebView wv = (WebView) findViewById(R.id.webView);
         wv.getSettings().setJavaScriptEnabled(true);
-        wv.setWebViewClient(new WebViewClient(){
-            @Override
-            public boolean shouldOverrideUrlLoading(WebView view, String url) {
-                if(isURLMatching(url)) {
-                    openCalEventActivity(url);
-                    return true;
-                }
-                return super.shouldOverrideUrlLoading(view, url);
-            }
-
-            @Override
-            public void onPageFinished(WebView view, String url) {
+        wv.setWebChromeClient(new WebChromeClient() {
+            public void onProgressChanged(WebView view, int progress) {
                 WebView wv = (WebView) findViewById(R.id.webView);
-                wv.loadUrl("javascript:" + getJavascriptForCalEvents());
-                wv.loadUrl("javascript: $('#loading').hide();");
+                TextView loadingText = (TextView)
+                        findViewById(R.id.loadingCalendarView);
+                ProgressBar pBar = (ProgressBar)
+                        findViewById(R.id.progressBarCalendarView);
+                pBar.setProgress(progress);
+                if (progress == 100){
+                    wv.setVisibility(View.VISIBLE);
+                    pBar.setVisibility(View.GONE);
+                    loadingText.setVisibility(View.GONE);
+                }
+                else {
+                    wv.setVisibility(View.GONE);
+                    pBar.setVisibility(View.VISIBLE);
+                    loadingText.setVisibility(View.VISIBLE);
+                }
             }
         });
 
@@ -73,15 +79,25 @@ public class HomeActivity extends Activity {
             Utils.displayErrorMessage(getString(R.string.Invalid_iCal_alert), this);
             this.events = new CalEvent[0];
         }
-
-        // Display initial data
         updateCalEventsInFullCalendar();
-        updateLastUpdatedLabel();
-    }
 
-    protected boolean isURLMatching(String url) {
-        // some logic to match the URL
-        return true;
+
+        // After loading is done, handle events in WebView
+        wv.setWebViewClient(new WebViewClient(){
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                openCalEventActivity(url);
+                return true;
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                WebView wv = (WebView) findViewById(R.id.webView);
+                wv.loadUrl("javascript:" + getJavascriptForCalEvents());
+                wv.loadUrl("javascript: $('#loading').hide();");
+                updateLastUpdatedLabel();
+            }
+        });
     }
 
     protected void openCalEventActivity(String url) {
@@ -183,8 +199,10 @@ public class HomeActivity extends Activity {
      */
     private void updateLastUpdatedLabel() {
         String dabbiLastUpdated = prefs.getString("lastUpdate","");
-        TextView t= (TextView)findViewById(R.id.lastUpdatedLabel);
-        t.setText(getString(R.string.lastUpdated) + " " + dabbiLastUpdated);
+        WebView wv = (WebView) findViewById(R.id.webView);
+        wv.loadUrl("javascript: $('#last-updated-label').html('"
+                + getString(R.string.lastUpdated) + " " + dabbiLastUpdated
+                + "');");
     }
 
     /**
@@ -229,10 +247,7 @@ public class HomeActivity extends Activity {
 
         @Override
         protected void onPostExecute(Void v) {
-            if (this.success) {
-                updateLastUpdatedLabel();
-            }
-            else {
+            if (!this.success) {
                 Utils.displayErrorMessage("Failed to load new data. Check your iCal URL in settings", sContext);
             }
             this.progress.dismiss();
@@ -248,7 +263,6 @@ public class HomeActivity extends Activity {
                     //Have to refresh screen after return from
                     //settings, as view might have been updated
                     events = dabbi.getAllCalEvents();
-                    updateLastUpdatedLabel();
                     updateCalEventsInFullCalendar();
                 }
             }
